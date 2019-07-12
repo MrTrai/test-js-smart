86065c25e27ed6ac7cbbda43fba49a052d0bf52f
trai tran <ttran96@gatech.edu>
2019-07-12 16:50:55 -0400

    add new error http request

---
+++ error2.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.get('/', function(req, res) {
+    res.send("id: " + req.query.id);
+    console.log("GET / id=" + req.query.id);
+});
+app.listen(3000);
+
