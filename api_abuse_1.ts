211194a4d9db34e1422ee8f3473e23f68db8c408
trai tran <ttran96@gatech.edu>
2019-07-23 16:54:26 -0400

    Add api abuse case 1

---
+++ api_abuse_1.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+function cb(msg = "", data = null) {
+}
+
+axios.get("foo")
+    .then((result) => {
+        if (result) {
+            cb(null, result);
+        } else {
+            cb("no-host-meta");
+        }
+    })
+    .catch((err) => {
+
+    });
